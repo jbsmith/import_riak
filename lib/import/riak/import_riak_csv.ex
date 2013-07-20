9053aad288a917da7b922589bf7dd6bf69e7055e
JB Smith <jbsmith@me.com>
2013-07-20 14:04:46 -0700

    further cleanup import_riak_csv.ex

--- lib/import/riak/import_riak_csv.ex
+++ lib/import/riak/import_riak_csv.ex
@@ -7,45 +7,54 @@ defmodule Import.Riak.Csv do
 	@max_lines 100000000
 
 	def file(filename, parser, start_index // 0, qty // 100000000, rate // 0) do
-		if(qty > @max_lines) do qty = @max_lines end
-		started_at = inspect(:erlang.localtime())
-		{:ok, riak_pid} = Import.Riak.init()
-		IO.write "\n#{inspect(self)} Seeking started at #{started_at}"
-		File.open(filename,[:read], 
-		  fn(file) ->
-		  	processlines(file, parser, riak_pid, start_index, qty, rate) 
-		  end
-		)
-		IO.write "\n#{inspect(self)} Completed at #{inspect(:erlang.localtime())}"
-		System.halt(0)
+	
+	    if(qty > @max_lines) do qty = @max_lines end
+	    {:ok, riak_pid} = Import.Riak.init()
+	    
+	    IO.write "\n#{inspect(self)} Seeking started at #{inspect(:erlang.localtime())}"
+	    
+	    File.open(filename,[:read], 
+	        fn(file) ->
+		    processlines(file, parser, riak_pid, start_index, qty, rate) 
+	        end
+	    )
+	    
+	    IO.write "\n#{inspect(self)} Completed at #{inspect(:erlang.localtime())}"
+	    System.halt(0)
+	    
 	end
 
         defp processlines(file, parser, rpid, start_index // 0, qty // 100000000, rate // 0) do
-		IO.puts "\n#{inspect(self)} Seeking line #{start_index} ..."
+
+	    IO.puts "\n#{inspect(self)} Seeking line #{start_index} ..."
+
 	    IO.stream(file)
 	    |> Stream.drop(start_index)
 	    |> Stream.with_index()
 	    |> Stream.map(
-	    	fn({line,idx}) ->
-			    if(idx==0) do IO.puts "\n#{inspect(self)} :#{start_index} Importing started at #{inspect(:erlang.localtime())}" end
-	    		ratelimit(idx, rate)
-	    		spawn(Import.Riak, :post, []) <- {self, parser.parse(rpid, :"ipgeo", line)}
-	    		receive do
-	    			{_,msg} ->
-	    				if(idx==0) do 
-			    			IO.puts "\n#{inspect(self)} key:#{msg} is first line processed"
-					    end
-	    				IO.write "\033[K\033[80D#{inspect(self)} key:#{msg} @line:#{1 + idx + start_index} processed"
-	    		end
+	    	 fn({line,idx}) ->
+	           if(idx==0) do IO.puts "\n#{inspect(self)} :#{start_index} Importing started at #{inspect(:erlang.localtime())}" end
+	    	   ratelimit(idx, rate)
+	    	   spawn(Import.Riak, :post, []) <- {self, parser.parse(rpid, :"ipgeo", line)}
+	    	   receive do
+	    	     {_,msg} ->
+	    	       if(idx==0) do 
+			 IO.puts "\n#{inspect(self)} key:#{msg} is first line processed"
+		       end
+	    	       IO.write "\033[K\033[80D#{inspect(self)} key:#{msg} @line:#{1 + idx + start_index} processed"
+	    	    end
 	    	 end
 	       )
 	    |> Enum.take(qty)
+
         end
 
         defp ratelimit(count, pause) do
+
     	    if(rem(count,10) == 0 && count>0) do 
 	        # pausing briefly 
-	    	:timer.sleep(pause)
+	        :timer.sleep(pause)
 	    end
+
         end
 end
